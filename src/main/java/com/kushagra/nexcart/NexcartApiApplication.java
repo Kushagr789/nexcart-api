0660134a8e7c60d56a3cdb84972e3c700c86edf5
Kushagra Saxena <saxenakush789@gmail.com>
2026-05-07 20:10:10 +0530

    First commit

---
+++ src/main/java/com/kushagra/nexcart/NexcartApiApplication.java
@@ -0,0 +1,13 @@
+package com.kushagra.nexcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexcartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexcartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushagra/nexcart/NexcartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushagra.nexcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexcartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
